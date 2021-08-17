83b8f838e65a8bfb16cfc28d658d7479c0101772
Deepu-000 <83580853+Deepu-000@users.noreply.github.com>
2021-08-17 18:45:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,9 +29,9 @@ class Bot(Client):
                 link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
                 self.invitelink = link
             except:
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning("Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                self.LOGGER(__name__).warning("<b> 😵 Bot can't Export Invite link from Force Sub Channel!<\b>")
+                self.LOGGER(__name__).warning("<b> 🙄Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission <\b>")
+                self.LOGGER(__name__).info("\n <b> Bot Stopped🤥.<\b>")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -40,14 +40,14 @@ class Bot(Client):
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning("Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+            self.LOGGER(__name__).warning("<b>😜Make Sure bot is Admin in DB Channel,☘️and Double check the CHANNEL_ID Value <\b>")
+            self.LOGGER(__name__).info("\nBot 🤥Stopped.")
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\n <b>Created by Dᴇᴇᴘᴜ<\b>")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
+        self.LOGGER(__name__).info("🤥Bot stopped.")

e5647408fe54e17971789c6f21215676bc5e2f61
Deepu-000 <83580853+Deepu-000@users.noreply.github.com>
2021-08-18 09:23:27 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,11 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>➥ Name</b> : <code> Auto Filter Bot</code>   
-<b>➥ Editor</b> : <b><i><a href="https://t.me/Deepu_The_Editor">ᴅᴇᴇᴘᴜ</a></i></b>
-<b>➥ Language</b> : <code>Python3</code>
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-<b>➥ Source Code</b> : <i><a href="https://github.com">Click Me</a></i>",
+            text = f"<b>➥ Name</b> : <code> Auto Filter Bot</code>\n\n<b>➥ Editor</b> : <b><i><a href="https://t.me/Deepu_The_Editor">ᴅᴇᴇᴘᴜ</a></i></b>\n\n<b>➥ Language</b> : <code>Python3</code>\n\n<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>\n\n<b>➥ Source Code</b> : <i><a href="https://github.com">Click Me</a></i>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [

09310b830e38f68640d0704b696369a0be9858e9
Deepu-000 <83580853+Deepu-000@users.noreply.github.com>
2021-08-18 08:56:43 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -14,7 +14,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
 <b>➥ Editor</b> : <b><i><a href="https://t.me/Deepu_The_Editor">ᴅᴇᴇᴘᴜ</a></i></b>
 <b>➥ Language</b> : <code>Python3</code>
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-<b>➥ Source Code</b> : <i><a href="https://github.com/Deepu-000/Fuckingshit">Click Me</a></i>",
+<b>➥ Source Code</b> : <i><a href="https://github.com">Click Me</a></i>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [

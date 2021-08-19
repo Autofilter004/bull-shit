79fdab37ccc2d114ea23c8f954a26fd9f130495d
Deepu-000 <83580853+Deepu-000@users.noreply.github.com>
2021-08-19 11:39:02 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -42,7 +42,7 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🦔Sʜᴀʀᴇ Uʀʟ🦔", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🚀Sʜᴀʀᴇ Uʀʟ🚀", url=f'https://telegram.me/share/url?url={link}')]])
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
